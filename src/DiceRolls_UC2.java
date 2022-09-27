08cf715f754fd4cfd81f4ace2fddeed83577c409
aoshinbm <aoshinbm28797@gmail.com>
2022-09-27 15:52:31 +0530

    Snake and Ladder roll the dice UC2 commit

--- src/DiceRolls_UC2.java
+++ src/DiceRolls_UC2.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class DiceRolls_UC2 {
+import java.util.Random;
+
+public class DiceRolls_UC2 {
+    public static void main(String[] args) {
+        System.out.println("Snake & Ladder Game");
+        Random rand = new Random();
+        int dice = rand. nextInt(7)%10;
+        System.out.println("Dice rolls "+dice);
+    }
 }
