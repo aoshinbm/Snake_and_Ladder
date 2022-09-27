0f0c80a3716c51be0eef29bf89b1240d6ff2607e
aoshinbm <aoshinbm28797@gmail.com>
2022-09-27 17:20:12 +0530

    changes updated

--- src/DiceRolls_UC2.java
+++ src/DiceRolls_UC2.java
@@ -3,8 +3,16 @@ import java.util.Random;
 public class DiceRolls_UC2 {
     public static void main(String[] args) {
         System.out.println("Snake & Ladder Game");
-        Random rand = new Random();
-        int dice = rand. nextInt(7)%10;
-        System.out.println("Dice rolls "+dice);
+        System.out.println("Starting position of the player is at 0");
+        int dice = (int)Math.floor(Math.random()*10)%7;
+        System.out.println("Dice roll: "+dice);
+        if(dice==6)
+        {
+            System.out.println("Start the game from START position");
+        }
+        else
+        {
+            System.out.println("Better luck next time");
+        }
     }
 }
--- src/SinglePlayer_UC1.java
+++ src/SinglePlayer_UC1.java
@@ -1,15 +1,35 @@
+import java.util.Random;
+
 public class SinglePlayer_UC1 {
     public static void main(String[] args) {
-        System.out.println("Snake & Ladder Game");
-        System.out.println("Starting position of the player is at 0");
-        int dice = (int)Math.floor(Math.random()*10)%7;
-        System.out.println("Dice roll: "+dice);
-        if(dice==6)
-        {
-            System.out.println("Start the game from START position");
+        int dice1,dice2,dice3,move;
+
+        Random rand = new Random();
+        System.out.println("Start the game");
+        dice1= rand.nextInt(7)%10;
+        System.out.println("Dice rolls "+dice1);
+        if(dice1==6) {
+            System.out.println("Start from 1st position i.e. START playing");
+            dice2 = rand.nextInt(7)%10;
+            System.out.println("Roll dice again "+dice2);
+            if(dice2==6) {
+                System.out.println("PLace next coin at the starting position");
+                dice3 = rand.nextInt(7)%10;
+                System.out.println("Roll dice again "+dice3);
+                if(dice3==6) {
+                    System.out.println("Triple 6 is equal to null.");
+                    System.out.println("STOP..!!!");
+                }
+                else {
+                    move=dice2+dice3;
+                    System.out.println("Move forward "+move+" steps");
+                }
+            }
+            else {
+                System.out.println("Move forward "+dice2+" steps");
+            }
         }
-        else
-        {
+        else {
             System.out.println("Better luck next time");
         }
     }
